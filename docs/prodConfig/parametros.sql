cb24dcc7d8a2dee71188a48897de0354d61c730d
MRodriguez08 <mau.rod.81090@gmail.com>
2014-10-01 20:13:54 -0300

    eliminados archivos innecesarios

--- docs/prodConfig/parametros.sql
+++
@@ -1,3 +0,0 @@
-
--- Parametro ruta base de archivos de la aplicacion
-update parametros set valor= '/hermes/bosnaweb04b/b1925/ipg.tinydevelop/files/riasAltas' where nombre='directorioBaseImagenes';
